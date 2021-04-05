980a29648656336429ab3d23ffeb86c5231707ba
ThatBlueDevil <70230904+ThatBlueDevil@users.noreply.github.com>
2021-04-05 18:35:41 -0300

    Create Bleus.lua

---
+++ Bleus.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 6052240333 or 6234730848 then
+  pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatBlueDevil/Bleus/main/FACILITY/Meep.lua"))()
+end)
+  else
+    print("[BLEUS] Player is not in Facilitiy!")
+end
