d9c2c0ece15af0dcc7dfaedf1da7b39cfcd64696
ThatBlueDevil <70230904+ThatBlueDevil@users.noreply.github.com>
2021-05-03 01:03:32 -0300

    Update source.lua

--- GIANT_SIM/source.lua
+++ GIANT_SIM/source.lua
@@ -7,13 +7,12 @@ local XX =
         Style = 1,
         SizeX = 400,
         SizeY = 400,
-        Theme = "Dark"
+        Theme = "Aqua",
+        ColorOverrides = {
+            MainFrame = Color3.fromRGB(35,35,35)
+        }
     }
 )
-
-local GoogleRemote = game:GetService("ReplicatedStorage").ReportGoogleAnalyticsEvent
-
--- Functions
 
 local s =
     setmetatable(
@@ -28,16 +27,41 @@ local s =
     }
 )
 
+local User = s["Players"].LocalPlayer
+local ReplicatedStorage = s["ReplicatedStorage"]
+
+local GoogleRemote = ReplicatedStorage.ReportGoogleAnalyticsEvent
+
 hookfunction(GoogleRemote.FireServer, function()
    return nil 
 end)
 
 function FindFolder(name)
-    for _,f in next, game:GetService("Workspace").Scene:GetChildren() do
+    for _,f in next, s["Workspace"].Scene:GetChildren() do
         if string.find(f.Name, name) then
            return f
         end
     end
+end
+
+function Eggs()
+    d = math.huge
+    e = nil
+    
+    local Egg = {}
+    
+    for i, v in ipairs(FindFolder("147"):GetChildren()) do
+        if (v:FindFirstChild("Prefab") and v["Prefab"].Position ~= nil) then
+            local Magnitude = (User.Character.HumanoidRootPart.Position - v.Position).Magnitude
+            
+            if (Magnitude < d) then
+                d = Magnitude
+                e = v
+            end
+        end
+    end
+    
+    return e
 end
 
 -- GUI
@@ -49,69 +73,63 @@ local Main =
     }
 )
 
-local Misc =
-    XX.New(
-    {
-        Title = "Misc"
-    }
-)
-
-local D =
+local lols =
     Main.Toggle(
     {
         Text = "Auto Farm",
         Callback = function(Value)
             _G.AutoFarm = Value
             while _G.AutoFarm do
-                wait(0.3)
-                local Noders = FindFolder("147"):GetChildren()
-                local s = math.random(1, #Noders)
-                game:GetService("RunService").RenderStepped:Wait()
-                tweenService, tweenInfo = game:GetService("TweenService"), TweenInfo.new(0, Enum.EasingStyle.Linear)
-                tween =
-                    tweenService:Create(
-                    game:GetService("Players")["LocalPlayer"].Character.HumanoidRootPart,
-                    tweenInfo,
-                    {CFrame = CFrame.new(Noders[s].Position)}
-                )
-                tween:Play()
+                
+            local Egg = Eggs()
+                
+            if (Egg) then
+                User.Character.Humanoid:ChangeState(11)
+                
+                if (User.Character.HumanoidRootPart.Position - Egg.Position).Magnitude > math.huge then L = 1 else L = .1 end
+                
+                tweenService, tweenInfo = s["TweenService"], TweenInfo.new(L, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut)
+                T = tweenService:Create(User.Character.HumanoidRootPart, tweenInfo, {CFrame = CFrame.new(Egg.Position)})
+                T:Play()
+                end
+                wait()
             end
         end,
         Enabled = false
     }
 )
 
-local blah =
-    Main.Toggle(
-    {
-        Text = "Auto Jump",
-        Callback = function(Value)
-            _G.AutoJump = Value
-            while _G.AutoJump do
-                wait(0.60)
-                local User = game.Players.LocalPlayer
-                local UserCharacter = User.Character
-                local Humanoid = UserCharacter.Humanoid
-                Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-            end
-        end,
-        Enabled = false
-    }
-)
-
-local blah =
+local lols =
     Main.Toggle(
     {
         Text = "Auto Attack",
         Callback = function(Value)
             _G.AutoAttack = Value
             while _G.AutoAttack do
+                s["ReplicatedStorage"].Aero.AeroRemoteServices.GameService.WeaponAttackStart:FireServer()
+                wait(.65)
+                s["ReplicatedStorage"].Aero.AeroRemoteServices.GameService.WeaponAnimComplete:FireServer()
+                
                 wait()
-                game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.GameService.WeaponAttackStart:FireServer()
-                wait(.10)
-                game:GetService("ReplicatedStorage").Aero.AeroRemoteServices.GameService.WeaponAnimComplete:FireServer()
             end
         end,
         Enabled = false
     }
 )
+
+spawn(function()
+    while true do
+        if (User.Character ~= nil and _G.AutoFarm) then
+            local function NoClipping()
+                for _, Parts in next, (User.Character:GetDescendants()) do
+                    if (Parts:IsA("BasePart") and Parts.CanCollide == true) then
+                        Parts.CanCollide = false
+                    end
+                end
+            end
+            
+            spawn(NoClipping)
+        end
+        wait()
+    end
+end)
