a2304e7383dba4e533f8eb72feabf0c248f92858
ThatBlueDevil <70230904+ThatBlueDevil@users.noreply.github.com>
2021-05-16 15:13:02 -0300

    Made it more cleaner (I havent tested yet)

--- DBZFS/NoSlow.lua
+++ DBZFS/NoSlow.lua
@@ -1,42 +1,23 @@
-coroutine.wrap(
-    function()
-        while wait() do
-            for i, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
-                if v.Name == "Justice Combination" then
-                    local action = game.Players.LocalPlayer.Character:WaitForChild("Action")
-                    if action then
-                        wait()
-                        action:Destroy()
-                    end
+function NoSlow()
+    local Destroyables = { "Action", "Attacking", "Using", "heavy", "hyper", "Tele", "tele", "Slow", "Killed", "KiBlasted", "MoveStart", "SuperAction", "Hyper", "Dodging", "KiBlasting", "BStun", "FShake", "Activity" }
+    
+    for i, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if table.find(Destroyables, v.Name) then
+            v:Destroy()
+            
+            if v.Name == "Justice Combination" then
+                local f = game.Players.LocalPlayer.Character:WaitForChild("Action")
+                
+                if f then
+                    f:Destroy()
                 end
-                if v.Name == "Attacking" then
-                    v:Destroy()
-                end
-                if v.Name == "Using" then
-                    v:Destroy()
-                end
-                if v.Name == "heavy" then
-                    v:Destroy()
-                end
-                if v.Name == "hyper" then
-                    v:Destroy()
-                end
-                if v.Name == "Tele" then
-                    v:Destroy()
-                end
-                if v.Name == "Slow" then
-                    v:Destroy()
-                end
-                if v.Name == "Action" then
-                    v:Destroy()
-                end
-                if v.Name == "Killed" then
-                    v:Destroy()
-                end
-                if v.Name == "Block" and v.Value == true then
-                    v.Value = false
-                end
+            end
+
+            if v.Name == "Block" and v.Value then
+                v.Value = false
             end
         end
     end
-)
+end
+
+coroutine.wrap(NoSlow)()
