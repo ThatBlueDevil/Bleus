5a4a295e03b5dfac1faea4fa2dd4f0e57193202b
Isela <70230904+ThatBlueDevil@users.noreply.github.com>
2021-08-26 20:57:58 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,8 @@
+local UrlBase = "https://raw.githubusercontent.com/ThatBlueDevil/Bleus/main/"
 local Games = {
-  [1] = { Name = "", Link = "" }
+  [1] = { Name = "", Link = UrlBase .. "" }
 }
- if (Games[game.PlaceId]) then
+
+if (Games[game.PlaceId]) then
   return loadstring(game:HttpGet(Games[game.PlaceId].Link)() -- coded this on my school device so this getting big rework when I'm home
- end
+end
