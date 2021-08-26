d8406b1abe7a20d97d42bd1ff7aefd2faba76b86
Isela <70230904+ThatBlueDevil@users.noreply.github.com>
2021-08-26 20:55:38 +0100

    Delete Bleus.lua

--- Bleus.lua
+++
@@ -1,7 +0,0 @@
-if game.PlaceId == 6052240333 or 6234730848 then
-  pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatBlueDevil/Bleus/main/FACILITY/Meep.lua"))()
-end)
-  else
-    print("[BLEUS] Player is not in Facilitiy!")
-end
