a726773c48871661bf727be7b85fdc2197055d94
Isela <70230904+ThatBlueDevil@users.noreply.github.com>
2021-08-26 20:55:27 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [1] = { Name = "", Link = "" }
+}
+ if (Games[game.PlaceId]) then
+  return loadstring(game:HttpGet(Games[game.PlaceId].Link)() -- coded this on my school device so this getting big rework when I'm home
+ end
