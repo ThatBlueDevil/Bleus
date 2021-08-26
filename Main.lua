d5f02e5a4bd3b0cda3e75497058d17fa38217d14
Isela <70230904+ThatBlueDevil@users.noreply.github.com>
2021-08-26 20:58:48 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,6 +3,6 @@ local Games = {
   [1] = { Name = "", Link = UrlBase .. "" }
 }
 
-if (Games[game.PlaceId]) then
-  return loadstring(game:HttpGet(Games[game.PlaceId].Link))() -- coded this on my school device so this getting big rework when I'm home
+if (Games[game.GameId]) then
+  return loadstring(game:HttpGet(Games[game.GameId].Link))() -- coded this on my school device so this getting big rework when I'm home
 end
