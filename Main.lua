c6a4431d2f4682787e6e9d170c100b1f9a6e45b7
Isela <70230904+ThatBlueDevil@users.noreply.github.com>
2021-09-02 11:07:21 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,7 @@
 local UrlBase = "https://raw.githubusercontent.com/ThatBlueDevil/Bleus/main/";
 local Games = {
-    [1335695570] = { Name = "Ninja Legends", Link = UrlBase .. "Ninja%20Legends/Source.lua" };
-    [1077961373] = { Name = "Giant Simulator", Link = UrlBase .. "GIANT_SIM/source.lua" };
+    [1335695570] = { Name = "Ninja Legends", Link = UrlBase .. "Ninja%20Legends/Source.lua" },
+    [1077961373] = { Name = "Giant Simulator", Link = UrlBase .. "GIANT_SIM/source.lua" }
 };
 
 assert(Games[game.GameId], "[BLEUS] This game isn't supported!");
