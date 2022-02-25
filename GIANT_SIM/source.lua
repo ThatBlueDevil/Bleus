d59a1ad8c8eec2cd46a2a64e6f54ff6ba2d353a4
ThatBlueDevil <70230904+ThatBlueDevil@users.noreply.github.com>
2022-02-24 19:48:39 -0500

    Update source.lua

--- GIANT_SIM/source.lua
+++ GIANT_SIM/source.lua
@@ -1,131 +1,116 @@
---[[ Author: Isela --]]
+--[[ 
+    Author: Scarlett
+    
+    Hope the little patch works, I might add a little bit more features but idk
+--]]
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))().Load({
-    Title = "Giant Simulator  ―  Bleus",
+local quick = loadstring(game:HttpGet('https://raw.githubusercontent.com/Belkworks/quick/master/init.lua'))();
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua'))().Load({
+    Title = 'Giant Simulator  ―  Bleus',
     Style = 1,
     SizeX = 400,
     SizeY = 400,
-    Theme = "Aqua",
+    Theme = 'Aqua',
     ColorOverrides = {
-        MainFrame = Color3.fromRGB(30, 30, 30)
+        MainFrame = Color3.fromRGB(35, 35, 35);
     }
-})
+}).New;
 
-local s = setmetatable({}, {
-    __index = function(self, key)
-        return game:GetService(key)
-    end
-})
+repeat wait() until (game and game:IsLoaded());
+local s = quick.Service;
 
--- Localization.
-local Player = s.Players.LocalPlayer;
-local Character = Player.Character or Player.CharacterAdded:Wait();
+local workspace = s.Workspace;
+local replicatedStorage = s.ReplicatedStorage;
+local currentCamera = workspace.CurrentCamera;
+local players = s.Players;
+local virtualUser = s.VirtualUser;
+local tweenService = s.TweenService;
+local runService = s.RunService;
+local stepped = runService.Stepped;
 
-Player.CharacterAdded:Connect(function(char)
-    Character = char;
+local client = players.LocalPlayer;
+local character = client.Character or client.CharacterAdded:Wait();
+client.CharacterAdded:Connect(function(char)
+    character = char;
 end);
 
-local Workspace = s.Workspace;
-local TweenService = s.TweenService;
-local RunService = s.RunService;
-local VirtualUser = s.VirtualUser;
-local ReplicatedStorage = s.ReplicatedStorage;
-
-local Camera = Workspace.CurrentCamera;
-local Heartbeat, Stepped, RenderStepped = RunService.Heartbeat, RunService.Stepped, RunService.RenderStepped;
-
--- Flags / oh my god I need a ui lib with actual flags :sob:.
-shared._Flags = {
-    AutoFarm,
-    AutoJump,
-    AutoAttack
-};
-
--- Anti-AFK and Anti-Logging(idk if they still have it)
 if getconnections then
-    for i = 1, #getconnections(Player.Idled) do
-        getconnections(Player.Idled)[i]:Disable();
+    for i = 1, #getconnections(client.Idled) do
+        getconnections(client.Idled)[i]:Disable();
     end;
 else
-    Player.Idled:Connect(function()
-       VirtualUser:Button2Down(Vector2.new(0, 0), CurrentCamera.CFrame);
+    client.Idled:Connect(function()
+       virtualUser:Button2Down(Vector2.new(), currentCamera.CFrame);
        wait(1);
-       VirtualUser:Button2Up(Vector2.new(0, 0), CurrentCamera.CFrame);
+       virtualUser:Button2Up(Vector2.new(), currentCamera.CFrame);
     end);
 end;
 
-hookfunction(ReplicatedStorage.ReportGoogleAnalyticsEvent.FireServer, function()
+replaceclosure(replicatedStorage.ReportGoogleAnalyticsEvent.FireServer, function()
    return nil;
 end);
 
--- Functions consist of: Tween | getNodeFolder | getANode.
-local Tween, getNodeFolder, getANode; do
-    function Tween(Obj, Properties, Time)
-        local Tweeb = TweenService:Create(Obj, TweenInfo.new(Time), Properties);
-        Tweeb:Play();
-        
-    	return Tweeb;
-    end;
 
-    function getNodeFolder()
-        for _, f in next, Workspace.Scene:GetChildren() do
-            if type(tonumber(f.Name)) == "number" then
-                return f;
-            end;
+local function tween(obj, time, ...)
+    local tween = tweenService:Create(obj, TweenInfo.new(time), ...);
+    tween:Play();
+    tween.Completed:Connect(function() tween:Destroy() end);
+	return tween;
+end;
+
+local function getNodeFolder()
+    for _, f in next, workspace.Scene:GetChildren() do
+        if type(tonumber(f.Name)) == 'number' then
+            return f;
         end;
-    end;
-    
-    function getANode()
-        local d, e = math.huge;
-        
-        for i, v in next, getNodeFolder():GetChildren() do
-            if (v:FindFirstChild("Prefab") and v.Prefab.Position) then
-                local Magnitude = Player:DistanceFromCharacter(v.Position);
-                
-                if (Magnitude < d) then
-                    d = Magnitude;
-                    e = v;
-                end;
-            end;
-        end;
-        
-        return e;
     end;
 end;
 
--- Library Initalation.
-local Main = Library.New({
+local function getANode()
+    local d, e = math.huge;
+    for _, v in next, getNodeFolder():GetChildren() do
+        if (v:FindFirstChild('Prefab') and #v.Prefab:GetChildren() > 0) then
+            local magnitude = client:DistanceFromCharacter(v.Position);
+            
+            if (magnitude <= d) then
+                d = magnitude;
+                e = v;
+            end;
+        end;
+    end;
+    return e;
+end;
+
+local main = lib({
     Title = "Main";
 }); do
-    Main.Toggle({
-        Text = "Auto Farm",
+    main.Toggle({
+        Text = 'Auto Farm',
         Callback = function(v)
-            shared._Flags.AutoFarm = v;
-        end,
-        
-        Enabled = false;
+            getgenv().autoFarmState = v;
+        end;
     });
 end;
 
-pcall(function() if shared._loop then shared._loop:Disconnect() end end);
-
-wait();
-task.spawn(function()
-    shared._loop = RenderStepped:Connect(function()
-        local Root = (Character.HumanoidRootPart or Character:WaitForChild("HumanoidRootPart"));
-        local Humanoid = (Character.Humanoid or Character:WaitForChild("Humanoid"));
-        
-        if shared._Flags.AutoFarm then
-            if getANode() then
-                Humanoid:ChangeState(11);
-                task.spawn(Tween, Root, {CFrame = getANode().CFrame}, Player:DistanceFromCharacter(getANode().Position) / 2500, Enum.EasingStyle.Linear);
-            end;
-            --
-            task.spawn(function()
-                ReplicatedStorage.Aero.AeroRemoteServices.GameService.WeaponAttackStart:FireServer();
-                wait(0.65);
-                ReplicatedStorage.Aero.AeroRemoteServices.GameService.WeaponAnimComplete:FireServer();
-            end);
+pcall(function() 
+    if getgenv().mainLoop then
+        getgenv().mainLoop:Disconnect();
+    end;
+end);
+getgenv().mainLoop = stepped:Connect(function()
+    if not character or (character and not (character:FindFirstChild('HumanoidRootPart') and character:FindFirstChild('Humanoid'))) then return end;
+    
+    if autoFarmState then
+        task.spawn(function()
+            replicatedStorage.Aero.AeroRemoteServices.GameService.WeaponAttackStart:FireServer();
+            wait(0.65);
+            replicatedStorage.Aero.AeroRemoteServices.GameService.WeaponAnimComplete:FireServer();
+        end);
+    
+        local node = getANode();
+        if node then
+            -- Did math.random because I was fresh out of brain juice LOL.
+            tween(character.HumanoidRootPart, client:DistanceFromCharacter(node.Position) / 2500, { CFrame = node.CFrame + Vector3.new(math.random(2, 5), 1, 0) }, Enum.EasingStyle.Linear);
         end;
-    end);
+    end;
 end);
