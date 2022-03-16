359cfd3252b2bb24937ea081a639180559c92fac
Scarlett <70230904+ThatBlueDevil@users.noreply.github.com>
2022-03-15 18:46:51 -0700

    Update Source.lua

--- SwordsManSimulator/Source.lua
+++ SwordsManSimulator/Source.lua
@@ -1,7 +1,7 @@
 --[[
     Author: Scarlett
     
-    I put little to no effort into this script, checks can be simplified and such more.
+    "Spawn killing has been patched!" 🤓
 --]]
 
 local quick = loadstring(game:HttpGet('https://raw.githubusercontent.com/Belkworks/quick/master/init.lua'))();
@@ -16,113 +16,104 @@ local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/Kinlei/Ma
     }
 }).New;
 
-repeat wait() until (game and game:IsLoaded());
 local s = quick.Service;
 
 local workspace = s.Workspace;
+local replicatedStorage = s.ReplicatedStorage;
 local players = s.Players;
 local runService = s.RunService;
 local stepped = runService.Stepped;
 
 local client = players.LocalPlayer;
 local character = client.Character or client.CharacterAdded:Wait();
+
 client.CharacterAdded:Connect(function(char)
     character = char;
 end);
 
-local function getWeapon(dir)
-    if not dir then return end;
-    return quick.find(dir:GetChildren(), function(v)
-        return v.Name:find('Sword');
-    end);
-end;
+local swordFuncs = require(replicatedStorage.Framework.Core.Shared.SwordFunctions);
 
 local function getStrength(player)
-    if not player or not player:FindFirstChild('leaderstats') then return end; -- Might be a useless check? I didn't put much thought into it.
-    local amount = quick.find(player.leaderstats:GetChildren(), function(v)
-        return v.Name:find('Strength');
-    end).Value;
-    return amount;
+    local strength = (player:FindFirstChild('leaderstats') and player.leaderstats:FindFirstChild('\240\159\146\170 Strength'));
+    return strength and strength.Value;
 end;
 
-local function autoSwing()
-    local weapon = getWeapon(character);
-    if not weapon then
-        weapon = getWeapon(client.Backpack);
-        if weapon then weapon.Parent = character end;
+local function autoSwing() -- complicated? I'll just improve ltr on
+    local sword = swordFuncs.GetBestSword(client).ModelPath.Name;
+    if not character:FindFirstChild(sword) then
+        client.Backpack[sword].Parent = character;
     end;
     
-    if (weapon and weapon:FindFirstChild('Slash', true)) then
-        weapon.Animation.Slash:FireServer();
+    character[sword]:Activate();
+end;
+
+local function getClosestPlayer()
+    local t, d = nil, math.huge;
+    for i = 1, #players:GetPlayers() do
+        local v = players:GetPlayers()[i];
+        if v ~= client and (v.Character and (v.Character and (v.Character:FindFirstChild('Humanoid') and v.Character:FindFirstChild('HumanoidRootPart')))) then
+            if v.Character.Humanoid.Health <= 0 then continue end;
+            if v.Character:FindFirstChild('ForceField') then continue end;
+            
+            do
+                local clientStrength = getStrength(client);
+                local targetStrength = getStrength(v);
+                if (clientStrength and targetStrength) and targetStrength > clientStrength then
+                    continue;
+                end;
+            end;
+            
+            local magnitude = client:DistanceFromCharacter(v.Character.HumanoidRootPart.Position);
+            if magnitude <= d then
+                t = v;
+                d = magnitude;
+            end;
+        end;
     end;
+    return t;
 end;
 
 local main = lib({
     Title = 'Main';
 }); do
     main.Toggle({
-        Text = 'Farm Kills',
+        Text = 'Auto Swing',
         Callback = function(state)
-            getgenv().farmKillsState = state;
+            getgenv().autoSwingState = state;
+            while autoSwingState do
+                autoSwing();
+                task.wait();
+            end;
         end;
     });
 
     main.Toggle({
-        Text = 'Auto Swing',
+        Text = 'Farm Kills',
         Callback = function(state)
-            getgenv().autoSwingState = state;
+            getgenv().farmKillsState = state;
+            while farmKillsState do
+                task.spawn(function()
+                    local target = getClosestPlayer();
+                    local targetRootPart = target.Character.HumanoidRootPart;
+                    
+                    character.HumanoidRootPart.CFrame = targetRootPart.CFrame + (targetRootPart.CFrame.lookVector * 2.5);
+                    if not autoSwingState then
+                        autoSwing();
+                    end;
+                end);
+                task.wait();
+            end;
         end;
     });
 
     main.Button({
-        Text = 'Invisibility',
+        Text = 'Anonymous',
         Callback = function()
-            if not character or (character and not character:FindFirstChild('HumanoidRootPart')) then return end;
-            local oldCFrame = character.HumanoidRootPart.CFrame;
-            character.HumanoidRootPart.CFrame = CFrame.new(256, 551, 312);
+            if not character then return end;
             
-            character.LowerTorso:BreakJoints();
-            character.HumanoidRootPart.CFrame = oldCFrame;
+            local billBoard = (character:FindFirstChild('Head') and character.Head:FindFirstChildWhichIsA('BillboardGui'));
+            if billBoard then billBoard:Destroy() end;
+            client:ClearCharacterAppearance();
         end;
     });
 end;
-
-pcall(function() 
-    if mainLoop then
-        mainLoop:Disconnect();
-    end;
-end);
-getgenv().mainLoop = stepped:Connect(function()
-    if not character or (character and not character:FindFirstChild('HumanoidRootPart')) then return end;
-    
-    if autoSwingState then
-        task.spawn(autoSwing);
-    end;
-    if farmKillsState then
-        local playerCollection = players:GetPlayers();
-        quick.each(playerCollection, function(v)
-            if not v.Character then return end;
-            local humanoid = v.Character:FindFirstChild('Humanoid');
-            if (humanoid and humanoid.Health <= 0) then return end;
-            
-            do -- ugly code no way!!!
-                local clientStrength = getStrength(client);
-                local targetStrength = getStrength(v);
-                if (clientStrength and targetStrength) then
-                    if targetStrength > clientStrength then return end;
-                end;
-            end;
-            
-            if v.Character:FindFirstChild('ForceField') then
-                return; -- Target has a forcefield, prevent attempt to damaging them.
-            end;
-            
-            if v.Character:FindFirstChild('HumanoidRootPart') then
-                local targetRootPart = v.Character.HumanoidRootPart;
-                character.HumanoidRootPart.CFrame = targetRootPart.CFrame + (targetRootPart.CFrame.lookVector * 2.5);
-                if not autoSwingState then autoSwing() end;
-            end;
-            task.wait();
-        end);
-    end;
-end);
